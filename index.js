a164630e0d04fc6ac8a964fed21aafc398ec71a0
jod35 <jodestrevin@gmail.com>
2019-11-12 16:02:23 +0300

    added 'fortune cookie' functionality to a module

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 const express= require('express');
 
 const app=express();
-const handlebars= require('express3-handlebars')
-                  .create({defaultLayout:'main'});
+const handlebars= require('express3-handlebars').create({defaultLayout:'main'});
+const fortune=require('./lib/fortune');
 
 app.engine('handlebars',handlebars.engine);
-app.set('viewengine','handlebars');
+app.set('view engine','handlebars');
+app.use(express.static(__dirname + '/public'));
 
 app.set('port',process.env.PORT || 3000);
-
 
 app.get('/',(req,res)=>{
       res.render('home');
 });
 
 app.get('/about',(req,res)=>{
-  res.render('about');
+ 
+  res.render('about',{fortune:fortune.getFortune()});
 });
 
 
+
 //these are middleware functions
-//custom 404 route
+//custom 404 catchall route 
 app.use((req,res)=>{
   res.type('text/plain');
   res.status(404);
   res.render('404');
 });
 
-//custom 505 route
+//custom 505 catchall route
 
 app.use((err,req,res)=>{
  res.type('text/plain');
