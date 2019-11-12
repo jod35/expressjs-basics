0ec600cf7e44be147d12a95b3c8fadcf731fdc50
jod35 <jodestrevin@gmail.com>
2019-11-12 14:21:03 +0300

    created a custom 404 and 500 route

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ app.listen(app.get('port'),()=>{
    console.log("Server is running at port 3000");
    
 });
+
+//custom 404 route
+app.use((req,res)=>{
+  res.type('text/plain');
+  res.status(404);
+  res.send("404 Not Found");
+});
+
+//custom 505 route
+
+app.use((err,req,res)=>{
+ res.type('text/plain');
+ res.status(500);
+ res.send("Internal Server Error!")
+});
