b605ee0a64e87fa789217eeb0b8c74a0f8fd3a4c
jod35 <jodestrevin@gmail.com>
2019-11-12 10:08:55 +0300

    created a middleware function

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
 const express=require('express');
 const path=require('path');
-
+const users=require('./members');
+const moment=require('moment');
 const PORT =process.env.PORT || 8080;
 
 const app=express();
-const users= [
-    {
-        name:"Jonathan",
-        age:20,
-        uni:"MUK"
-    },
-    {
-        name:"Jonathan",
-        age:20,
-        uni:"MUK"
-    },
-    {
-        name:"Jon",
-        age:17,
-        uni:"MUK"
-    },
-    {
-        name:"Jodes",
-        age:24,
-        uni:"MUK"
-    },
-];
 
+//a middleware
+
+const logger= (req,res,next)=>{
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}${moment().format()}`);
+    next();
+    
+};
+
+//makes a static directory 'public'
 app.use(express.static(path.join(__dirname,'public')));
 
+//init logger
+app.use(logger);
+
+//simple api route returns json 
 app.get('/api/members',(req,res)=>{
   res.json(users);
 });
+
+//starts our server on the specified port
 app.listen(PORT,()=>{
   console.log(`Server running on port ${PORT}`);
 
---
+++ members.js
@@ -0,0 +1,24 @@
+const users= [
+    {
+        name:"Jonathan",
+        age:20,
+        uni:"MUK"
+    },
+    {
+        name:"Jonathan",
+        age:20,
+        uni:"MUK"
+    },
+    {
+        name:"Jon",
+        age:17,
+        uni:"MUK"
+    },
+    {
+        name:"Jodes",
+        age:24,
+        uni:"MUK"
+    },
+];
+
+module.exports=users;
