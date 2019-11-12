e8d9315d35a6c2972828f44e95bff8b003aedc3d
jod35 <jodestrevin@gmail.com>
2019-11-12 09:50:23 +0300

    made the simple rest api

--- index.js
+++ index.js
@@ -4,10 +4,35 @@ const path=require('path');
 const PORT =process.env.PORT || 8080;
 
 const app=express();
+const users= [
+    {
+        name:"Jonathan",
+        age:20,
+        uni:"MUK"
+    },
+    {
+        name:"Jonathan",
+        age:20,
+        uni:"MUK"
+    },
+    {
+        name:"Jon",
+        age:17,
+        uni:"MUK"
+    },
+    {
+        name:"Jodes",
+        age:24,
+        uni:"MUK"
+    },
+];
 
 app.use(express.static(path.join(__dirname,'public')));
 
+app.get('/api/members',(req,res)=>{
+  res.json(users);
+});
 app.listen(PORT,()=>{
   console.log(`Server running on port ${PORT}`);
-  
+
 });
