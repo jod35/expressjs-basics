0e74a6db7bf92c3874e8a6458e3a23901b7fefbc
jod35 <jodestrevin@gmail.com>
2019-11-12 09:32:16 +0300

    rendered some HTML

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express=require('express');
+const path=require('path');
 
-const app =express();
-const PORT= process.env.PORT || 5000;
+const PORT =process.env.PORT || 8080;
+
+const app=express();
 
 app.get('/',(req,res)=>{
-    res.send("Hello World");
+   res.sendFile(path.join(__dirname,'public/index.html'));
 });
 
 app.listen(PORT,()=>{
-    console.log("Server running on port 5000");
-    
+  console.log(`Server running on port ${PORT}`);
+  
 });
